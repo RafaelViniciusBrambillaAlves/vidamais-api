db703853cb11bc9421f775511482287245320a8b
Rafael Vinicius Brambilla Alves <rafael.brambilla3@gmail.com>
2025-04-19 11:34:25 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Dockerfile
+FROM node:18-alpine AS base
+
+WORKDIR /app
+
+# Etapa de construção
+FROM base AS build
+COPY package*.json ./
+RUN npm ci
+COPY . .
+RUN npm run build
+
+# Etapa de produção
+FROM base AS production
+ENV NODE_ENV=production
+COPY --from=build /app/dist ./dist
+COPY --from=build /app/node_modules ./node_modules
+COPY package*.json ./
+
+EXPOSE 3000
+CMD ["npm", "run", "start:dev"]
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './Services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: "postgres",
+      password: "12345",
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
